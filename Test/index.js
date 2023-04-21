4ddd7aa00f042140599d3fefaed2122637190ff1
Abigail Anyating <Abigailanyating@outlook.com>
2023-04-21 23:44:33 +0100

    fitness test

---
+++ Test/index.js
@@ -0,0 +1,48 @@
+const img1 = document.getElementById("one");
+
+img1.addEventListener("click", (e)=>
+{
+e.target.style.backgroundColor="green"
+})
+const img2 = document.getElementById("two");
+
+img2.addEventListener("click", (e)=>
+{
+e.target.style.backgroundColor="blue"
+})
+const img3 = document.getElementById("three");
+
+img3.addEventListener("click", (e)=>
+{
+e.target.style.backgroundColor="yellow"
+})
+const img4 = document.getElementById("four");
+
+img4.addEventListener("click", (e)=>
+{
+e.target.style.backgroundColor="red"
+})
+const img5 = document.getElementById("five");
+
+img5.addEventListener("click", (e)=>
+{
+e.target.style.backgroundColor="purple"
+})
+const img6 = document.getElementById("six");
+
+img6.addEventListener("click", (e)=>
+{
+e.target.style.backgroundColor="pink"
+})
+const img7 = document.getElementById("seven");
+
+img7.addEventListener("click", (e)=>
+{
+e.target.style.backgroundColor="black"
+})
+const img8 = document.getElementById("eight");
+
+img8.addEventListener("click", (e)=>
+{
+e.target.style.backgroundColor="grey"
+})
